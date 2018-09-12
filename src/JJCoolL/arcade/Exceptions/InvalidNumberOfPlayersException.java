40870e4892213bf6d3fc146507104b2b0bf788c8
Jodie Thomas <jodie.elizabeth.thomas@outlook.com>
2018-09-12 16:28:31 +0100

    Comment made - Uno Class, further changes 2

--- src/JJCoolL/arcade/Exceptions/InvalidNumberOfPlayersException.java
+++ src/JJCoolL/arcade/Exceptions/InvalidNumberOfPlayersException.java
@@ -1,4 +1,15 @@
 package JJCoolL.arcade.Exceptions;
 
-public class InvalidNumberOfPlayersException implements Exception {
+public class InvalidNumberOfPlayersException extends Exception {
+
+    public InvalidNumberOfPlayersException() {
+
+    }
+    public InvalidNumberOfPlayersException(String message) {
+        super(message);
+    }
+
+    public InvalidNumberOfPlayersException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
